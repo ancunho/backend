fb087d4441e0f956fce6ccc042c6223c5d946a8e
cunho <ancunho@naver.com>
2022-08-19 23:30:54 +0800

    captcha추가
    Filter, Controller등 추가

--- src/main/java/online/buza/blog/controller/admin/AdminCommonController.java
+++ src/main/java/online/buza/blog/controller/admin/AdminCommonController.java
@@ -1,27 +1,74 @@
 package online.buza.blog.controller.admin;
 
 import com.github.pagehelper.PageHelper;
+import com.google.code.kaptcha.Producer;
 import lombok.extern.slf4j.Slf4j;
 import online.buza.blog.annotation.AdminUserLogin;
+import online.buza.blog.annotation.PassLogin;
 import online.buza.blog.common.BaseRequest;
 import online.buza.blog.common.BaseResponse;
+import online.buza.blog.common.Const;
 import online.buza.blog.dto.SysUserDto;
 import online.buza.blog.service.AdminUserService;
+import online.buza.blog.util.RedisUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import sun.misc.BASE64Encoder;
 
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/common")
 public class AdminCommonController {
 
-    @Autowired
     private AdminUserService adminUserService;
+    private Producer producer;
+    public RedisUtil redisUtil;
+
+    public AdminCommonController(AdminUserService adminUserService, Producer producer, RedisUtil redisUtil) {
+        this.adminUserService = adminUserService;
+        this.producer = producer;
+        this.redisUtil = redisUtil;
+    }
+
+    @PassLogin
+    @GetMapping("/captcha")
+    public BaseResponse captcha(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String captchaCode = producer.createText();
+        String captchaKey = UUID.randomUUID().toString();
+
+        // For Test
+//        code = "code111";
+//        captchaKey="key111";
+
+        BufferedImage image = producer.createImage(captchaCode);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ImageIO.write(image, "jpg", outputStream);
+        BASE64Encoder encoder = new BASE64Encoder();
+        String str = "data:image/jpeg;base64,";
+        String base64Img = str + encoder.encode(outputStream.toByteArray());
+
+        // Redis에 저장
+        redisUtil.hset(Const.CAPTCHA_KEY, captchaKey, captchaCode, 120);
+        log.info("验证码 -- {} - {}", captchaKey, captchaCode);
+
+        Map<String, Object> returnMap = new HashMap<>();
+        returnMap.put("captchaCode", captchaCode);
+        returnMap.put("captchaKey", captchaKey);
+        returnMap.put("base64Img", base64Img);
+
+        return BaseResponse.valueOfSuccess(returnMap);
+    }
+
 
     @AdminUserLogin
     @PostMapping(value = "/user/list")
--- src/main/java/online/buza/blog/filter/CaptchaFilter.java
+++ src/main/java/online/buza/blog/filter/CaptchaFilter.java
@@ -5,15 +5,16 @@ import lombok.extern.slf4j.Slf4j;
 import online.buza.blog.common.BaseResponse;
 import online.buza.blog.common.Const;
 import online.buza.blog.exception.CaptchaException;
+import online.buza.blog.util.Box;
+import online.buza.blog.util.HttpUtility;
+import online.buza.blog.util.PropertiesUtils;
 import online.buza.blog.util.RedisUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -23,39 +24,49 @@ import java.nio.charset.StandardCharsets;
  * 图片验证码校验过滤器，在登录过滤器前
  * */
 @Slf4j
-@Component
-public class CaptchaFilter extends OncePerRequestFilter {
+@WebFilter(filterName = "captchaFilter", urlPatterns = "/adminLoginProc")
+public class CaptchaFilter implements Filter {
 
     @Autowired
     private RedisUtil redisUtil;
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String url = request.getRequestURI();
-
-        if (url.contains(PropertiesUtils.getAdminLoginUrl()) && request.getMethod().equals("POST")) {
-            //1. 校验验证码
-            try {
-                this.validateCaptcha(request);
-            } catch (CaptchaException e) {
-                //交给认证失败处理器
-                response.setContentType("application/json;charset=UTF-8");
-                ServletOutputStream outputStream = response.getOutputStream();
-
-                BaseResponse serverResponse = BaseResponse.valueOfFailureMessage(Const.Message.CAPTCHAR_KEY_ERROR);
-                outputStream.write(JSONObject.toJSONString(serverResponse).getBytes(StandardCharsets.UTF_8));
-
-                outputStream.flush();
-                outputStream.close();
-            }
-        }
-
-        filterChain.doFilter(request, response);
-    }
+//    @Override
+//    protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+//        String url = request.getRequestURI();
+//        ServletOutputStream outputStream = null;
+//
+//        if (url.contains(PropertiesUtils.getAdminLoginUrl()) && request.getMethod().equals("POST")) {
+//            //1. 校验验证码
+//            try {
+//                this.validateCaptcha(request);
+//            } catch (CaptchaException e) {
+//                //交给认证失败处理器
+//                response.setContentType("application/json;charset=UTF-8");
+//                outputStream = response.getOutputStream();
+//
+//                BaseResponse serverResponse = BaseResponse.valueOfFailureMessage(Const.Message.CAPTCHAR_KEY_ERROR);
+//                outputStream.write(JSONObject.toJSONString(serverResponse).getBytes(StandardCharsets.UTF_8));
+//
+//                outputStream.flush();
+//
+//            } finally {
+//                if (outputStream != null) {
+//                    try {
+//                        outputStream.close();
+//                    } catch (Exception e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            }
+//        }
+//
+//        filterChain.doFilter(request, response);
+//    }
 
     private void validateCaptcha(HttpServletRequest request) {
-        String captchaCode = request.getParameter("captchaCode");
-        String captchaKey = request.getParameter("captchaKey"); //captchaKey
+        Box box = HttpUtility.getBox(request);
+        String captchaCode = box.get("captchaCode");
+        String captchaKey = box.get("captchaKey"); //captchaKey
 
         if (StringUtils.isBlank(captchaCode) || StringUtils.isBlank(captchaKey)) {
             throw new CaptchaException("验证码不能为空");
@@ -66,5 +77,40 @@ public class CaptchaFilter extends OncePerRequestFilter {
         }
 
         redisUtil.hdel(Const.CAPTCHA_KEY, captchaKey);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        String url = request.getRequestURI();
+        ServletOutputStream outputStream = null;
+
+        if (url.contains(PropertiesUtils.getAdminLoginUrl()) && request.getMethod().equals("POST")) {
+            //1. 校验验证码
+            try {
+                this.validateCaptcha(request);
+            } catch (CaptchaException e) {
+                //交给认证失败处理器
+                response.setContentType("application/json;charset=UTF-8");
+                outputStream = response.getOutputStream();
+
+                BaseResponse serverResponse = BaseResponse.valueOfFailureMessage(Const.Message.CAPTCHAR_KEY_ERROR);
+                outputStream.write(JSONObject.toJSONString(serverResponse).getBytes(StandardCharsets.UTF_8));
+
+                outputStream.flush();
+
+            } finally {
+                if (outputStream != null) {
+                    try {
+                        outputStream.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        filterChain.doFilter(request, response);
     }
 }
