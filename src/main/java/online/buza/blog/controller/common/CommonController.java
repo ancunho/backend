f495c52c62d8eed5c23a8b9568f0b6a5ebc13dc6
cunho <ancunho@naver.com>
2022-08-21 22:18:13 +0800

    pubPrintOut추가

--- src/main/java/online/buza/blog/controller/common/CommonController.java
+++ src/main/java/online/buza/blog/controller/common/CommonController.java
@@ -1,10 +1,17 @@
 package online.buza.blog.controller.common;
 
+import com.google.gson.GsonBuilder;
 import lombok.extern.slf4j.Slf4j;
 import online.buza.blog.service.CommonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 
 @Slf4j
 @RestController
@@ -13,5 +20,36 @@ public class CommonController {
 
     @Autowired
     private CommonService commonService;
+
+    @Autowired
+    public HttpServletRequest request;
+
+    @Autowired
+    public HttpServletResponse response;
+
+    public void putPrintOut(Object obj) {
+        try {
+            response.setCharacterEncoding("UTF-8");
+            PrintWriter pw = response.getWriter();
+            pw.print(new GsonBuilder().serializeNulls().create().toJson(obj));
+            pw.flush();
+            pw.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void putPrintOut(String msg) {
+        try {
+            response.setCharacterEncoding("UTF-8");
+            PrintWriter pw = response.getWriter();
+            pw.print(new GsonBuilder().serializeNulls().create().toJson(msg));
+            pw.flush();
+            pw.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 
 }
