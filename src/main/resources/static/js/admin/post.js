e652fab42077809b0ad4bf8c8a1688e8dc171813
cunho <ancunho@naver.com>
2022-08-23 15:20:23 +0800

    Update post.js

--- src/main/resources/static/js/admin/post.js
+++ src/main/resources/static/js/admin/post.js
@@ -1,5 +1,6 @@
 
 /**
+ * Create Post js
  * Init Data
  * @returns {{editor: null, lstPostType: *[], editorData: string, form: {isJoin: boolean, eventDateRange: string, postContent: string, postType: string, postPrice: number, eventEndTime: string, postTitle: string, eventStartTime: string, postId: string, type: *[], isNeedPay: boolean, status: string}, save(): void, loading: boolean}}
  */
@@ -31,16 +32,24 @@ function initData() {
                 .then(res => {
                     var data = res.data;
                     if (data.code !== 0) {
-                        swal("Error", data.msg, "error")
-                            .then((value => {
-                               console.log("error");
-                                _this.form.postTitle = '';
-                                editor.setHtml('');
-                            }));
+                        swal({
+                            title: "Error!",
+                            text: data.msg,
+                            icon: "error",
+                            closeOnClickOutside: false,
+                        }).then(value => {
+                            console.log("error");
+                            _this.form.postTitle = '';
+                            editor.setHtml('');
+                        });
                         return;
                     } else {
-                        swal("Success Save!", data.msg, "success")
-                            .then(value => {
+                        swal({
+                            title: "Success Save!",
+                            text: data.msg,
+                            icon: "success",
+                            closeOnClickOutside: false,
+                        }).then(value => {
                                 console.log("success");
                                 _this.form.postTitle = '';
                                 editor.setHtml('');
