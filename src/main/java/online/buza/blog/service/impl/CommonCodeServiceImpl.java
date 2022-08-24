88f2d48c98656f5807946a3c6f70cd3df7c909e0
cunho <ancunho@naver.com>
2022-08-24 17:17:28 +0800

    Create CommonCodeServiceImpl.java

---
+++ src/main/java/online/buza/blog/service/impl/CommonCodeServiceImpl.java
@@ -0,0 +1,59 @@
+package online.buza.blog.service.impl;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import online.buza.blog.dao.TbCommonCodeMapper;
+import online.buza.blog.dto.TbCommonCodeDto;
+import online.buza.blog.entity.TbCommonCode;
+import online.buza.blog.service.CommonCodeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Slf4j
+@Getter
+@Setter
+@AllArgsConstructor
+@Service
+public class CommonCodeServiceImpl implements CommonCodeService {
+
+    @Autowired
+    private TbCommonCodeMapper tbCommonCodeMapper;
+
+    @Transactional
+    public Boolean insertTbCommonCode(TbCommonCode tbCommonCode) {
+        int insertCount = tbCommonCodeMapper.insertSelective(tbCommonCode);
+        if (insertCount > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    @Transactional
+    public Boolean updateTbCommonCode(TbCommonCode tbCommonCode) {
+        int updateCount = tbCommonCodeMapper.updateByPrimaryKeySelective(tbCommonCode);
+        if (updateCount > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public List<TbCommonCodeDto> getAllTbCommonCodeList() {
+        return tbCommonCodeMapper.getAllTbCommonCodeList();
+    }
+
+    public TbCommonCodeDto getTbCommonCodeInfoByCodeId(Integer codeId) {
+        return tbCommonCodeMapper.getTbCommonCodeInfoByCodeId(codeId);
+    }
+
+    public List<TbCommonCodeDto> lstTbCommonCodeByCodeType(String codeType) {
+        return tbCommonCodeMapper.lstTbCommonCodeByCodeType(codeType);
+    }
+
+
+
+}
