6fe6c74054486e171d36a419e8d65d679ffe176c
cunho <ancunho@naver.com>
2022-08-24 17:22:02 +0800

    Create setting.js

---
+++ src/main/resources/static/js/admin/setting.js
@@ -0,0 +1,22 @@
+
+/**
+ * Create Setting js
+ * Init Data
+ */
+function initCommonCodeIndex() {
+    return {
+        itemList: [],
+        modifyItem: {},
+        loading: true,
+        currentPage: 1, //page
+        pageSize: 15, //limit
+        pageSizes: [15, 30, 50, 100],
+        total: 100,
+        isModalVisible: false,
+        buzaModalTitle: 'Modal',
+    }
+}
+
+
+
+
