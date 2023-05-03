f220e56e5090f7781dcae03d3c3bf688198185bb
cunho <ancunho@naver.com>
2023-05-03 14:31:52 +0800

    test

--- document/CREATE.sql
+++ document/CREATE.sql
@@ -9,7 +9,7 @@ CREATE TABLE `ma_city` (
                            `CITY_ENAME` varchar(45) DEFAULT NULL,
                            PRIMARY KEY (`CITY_ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='城市表';
-
+-- asdfasdfsadf
 
 -- ma_province table
 CREATE TABLE `ma_province` (
